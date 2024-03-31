95d63ceac34c9e1e6baea2d947a9f5176e4df95d
tingyuan <1932294867@qq.com>
2024-03-31 12:48:47 +0800

    -

--- types.d.ts
+++ types.d.ts
@@ -1,45 +1,68 @@
-import type {
-	StyleProp, ViewStyle, TextStyle, ImageStyle,
-} from 'react-native';
+import type { StyleProp, ViewStyle, TextStyle, ImageStyle } from 'react-native'
 
 declare global {
-	function tw(classes: string, ...styles: Array<StyleProp<ViewStyle | TextStyle | ImageStyle>>): any; // StyleProp<ViewStyle | TextStyle | ImageStyle>
+  function tw(classes: string, ...styles: Array<StyleProp<ViewStyle | TextStyle | ImageStyle>>): any // StyleProp<ViewStyle | TextStyle | ImageStyle>
 }
 
 // Copy from nativewind: https://github.com/marklawlor/nativewind/blob/main/packages/react-native-css-interop/types.d.ts
+import {
+  ScrollViewProps,
+  ScrollViewPropsAndroid,
+  ScrollViewPropsIOS,
+  Touchable,
+  VirtualizedListProps,
+} from 'react-native'
+
+declare module '@react-native/virtualized-lists' {
+  export interface VirtualizedListWithoutRenderItemProps<ItemT> extends ScrollViewProps {
+    ListFooterComponentClassName?: string
+    ListHeaderComponentClassName?: string
+  }
+}
+
 declare module 'react-native' {
-	type FlatListProperties<ItemT> = {
-		className?: string;
-		tw?: string;
-	} & VirtualizedListProps<ItemT>;
-
-	type ImagePropertiesBase = {
-		className?: string;
-		tw?: string;
-	};
-
-	type ViewProperties = {
-		className?: string;
-		tw?: string;
-	};
-
-	type TextProperties = {
-		className?: string;
-		tw?: string;
-	};
-
-	type SwitchProperties = {
-		className?: string;
-		tw?: string;
-	};
-
-	type InputAccessoryViewProperties = {
-		className?: string;
-		tw?: string;
-	};
-
-	type TouchableWithoutFeedbackProperties = {
-		className?: string;
-		tw?: string;
-	};
+  interface ScrollViewProps
+    extends ViewProps,
+      ScrollViewPropsIOS,
+      ScrollViewPropsAndroid,
+      Touchable {
+    contentContainerClassName?: string
+    indicatorClassName?: string
+  }
+  interface FlatListProps<ItemT> extends VirtualizedListProps<ItemT> {
+    columnWrapperClassName?: string
+  }
+  interface ImageBackgroundProps extends ImagePropsBase {
+    imageClassName?: string
+  }
+  interface ImagePropsBase {
+    className?: string
+  }
+  interface ViewProps {
+    className?: string
+  }
+  interface TextInputProps {
+    placeholderClassName?: string
+  }
+  interface TextProps {
+    className?: string
+  }
+  interface SwitchProps {
+    className?: string
+  }
+  interface InputAccessoryViewProps {
+    className?: string
+  }
+  interface TouchableWithoutFeedbackProps {
+    className?: string
+  }
+  interface StatusBarProps {
+    className?: string
+  }
+  interface KeyboardAvoidingViewProps extends ViewProps {
+    contentContainerClassName?: string
+  }
+  interface ModalBaseProps {
+    presentationClassName?: string
+  }
 }
